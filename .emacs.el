abec3914dec7a2d4c90c82228a74c184ad7f8873
tsu-nera <fox10225fox@gmail.com>
2020-10-05 13:09:19 +0900

    update

--- .emacs.el
+++ .emacs.el
@@ -63,4 +63,22 @@
   (recentf-mode 1))
 (global-set-key (kbd "C-x C-r") 'recentf-open-files)
 
+;;;
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(package-initialize)
+
 ;; end config
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(markdown-mode ##)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
